4303d3db2ed33e8b3c5cb495b606682d9150da48
Rafael S Santos <rafael.silva.sts@gmail.com>
2019-07-19 16:39:24 -0300

    Init experiment

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -11,7 +11,7 @@ do
 	./parallel-hybrid-sort -f "data/${nElems}" -n "$nTotalElems" -m "$nMax" -l > "data/seqlog_${nElems}_log.csv"
 	for (( k = 1; k<=10; k++))
 	do
-		./parallel-hybrid-sort -f "data/${nElems}" -i "data/${nElems}input.txt" -m "$nMax" -l >> "data/seqlog_${nElems}_log.csv"
+		mpirun -n 1 ./parallel-hybrid-sort -f "data/${nElems}" -i "data/${nElems}input.txt" -l >> "data/seqlog_${nElems}_log.csv"
 	done
 done
 
@@ -21,17 +21,17 @@ done
 
 for (( i=1; i<=5; i++ ))
 do
-	nThreads=$((2**$i))
+	nProcess=$((2**$i))
 		
 	for (( j=0; j<=3; j++ ))
 	do
 		nElems=$((200*2**j))
 		nTotalElems=$(($nElems * 32))
 		
-		#./parallel-hybrid-sort -f "data/${nElems}" -n "$nTotalElems" -m "$nMax" -t "$nThreads" -l -p > "data/log_${nElems}_${nThreads}_log.csv"
+		#./parallel-hybrid-sort -f "data/${nElems}" -n "$nTotalElems" -m "$nMax" -t "$nProcess" -l -p > "data/log_${nElems}_${nProcess}_log.csv"
 		for (( k = 1; k<=10; k++))
 		do
-			./parallel-hybrid-sort -f "data/${nElems}" -i "data/${nElems}input.txt" -m "$nMax" -t "$nThreads" -l -p >> "data/log_${nElems}_${nThreads}_log.csv"
+			mpirun -n 32 ./parallel-hybrid-sort -f "data/${nElems}" -i "data/${nElems}input.txt" -t "$nProcess" -l -p >> "data/log_${nElems}_${nThreads}_log.csv"
 		done
 	done
 done
